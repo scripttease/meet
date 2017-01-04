f2aa09859f2355773383ad642edd04c114b46148
Alice Dee <amyalicedee@gmail.com>
2017-01-04 22:22:51 +0000

    Correct aws region

--- infrastruture/main.tf
+++ infrastruture/main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-2"
 }
-
-# data "terraform_remote_state" "master_state" {
-#   backend = "s3"
-#   config {
-#     bucket = "meet-app--terraform-state"
-#     region = "eu-west-2"
-#     key = "meet-app--${var.env}.tfstate"
-#   }
-# }
